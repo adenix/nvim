7765f163f08737e99b5926cde756cb47c77d3c52
Austin Nicholas <adenix@Austins-MacBook-Pro.local>
2022-05-22 04:59:23 -0500

    Converted to lua and reconfigured everything

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,10 @@
+vim.cmd [[
+try
+  let g:sonokai_style = 'andromeda'
+  let g:sonokai_better_performance = 1
+  colorscheme sonokai
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
---
+++ lua/user/keymaps.lua
@@ -0,0 +1,82 @@
+-- Options
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- NVimTree
+keymap("n", "<leader>e", ":NVimTreeToggl<CR>", opts)
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Telescope
+keymap("n", "<c-f>", ":Telescope find_files<CR>", opts)
+
+-- Save with CTRL-s
+keymap("n", "<c-s>", ":w<CR>", opts)
+
+-- Close
+keymap("n", "<c-w>", ":bp<bar>sp<bar>bn<bar>bd<CR>", opts)
+keymap("n", "<c-q>", ":qall<CR>", opts)
+keymap("n", "<S-w>", ":bp<bar>sp<bar>bn<bar>bd!<CR>", opts)
+keymap("n", "<S-q>", ":qall!<CR>", opts)
+
+-- LSP Null LS Format
+keymap("n", "<leader>f", ":Format<CR>", opts)
+
+-- Resize with arrows
+keymap("n", "<S-Up>", ":resize +2<CR>", opts)
+keymap("n", "<S-Down>", ":resize -2<CR>", opts)
+keymap("n", "<S-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<S-Right>", ":vertical resize +2<CR>", opts)
+
+-- Move text up and down (Option-jk)
+keymap("n", "∆", "<Esc>:m .+1<CR>==", opts)
+keymap("n", "˚", "<Esc>:m .-2<CR>==", opts)
+
+-- Toggle Terminal
+keymap("n", "<leader>g", ":lua _LAZYGIT_TOGGLE()<CR>", opts)
+keymap("n", "<leader>j", ":lua _NODE_TOGGLE()<CR>", opts)
+keymap("n", "<leader>h", ":lua _HTOP_TOGGLE()<CR>", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Save with CTRL-s
+keymap("i", "<c-s>", "<ESC>:w<CR>i", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down (Options-jk)
+keymap("v", "∆", ":m .+1<CR>==", opts)
+keymap("v", "˚", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "∆", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "˚", ":move '<-2<CR>gv-gv", opts)
